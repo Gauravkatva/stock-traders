57ea23e4125d9e9abcef1bc0d40d423ca38952b6
Gaurav Katva <53577224+Gauravkatva@users.noreply.github.com>
2021-08-19 15:55:12 +0530

    home screen ui updated

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -16,6 +16,18 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final Auth _auth = Auth();
+  Color? _tileColor(String value) {
+    Color? color = Colors.white;
+    if (value.toUpperCase() == "SUCCESS") {
+      color = Colors.green[200];
+    } else if (value.toUpperCase() == "FAILURE") {
+      color = Colors.red[200];
+    } else if (value.toUpperCase() == "ONGOING") {
+      color = Colors.yellow[200];
+    }
+    return color;
+  }
+
   @override
   Widget build(BuildContext context) {
     List _stockList = Provider.of<List<Stock>>(context);
@@ -52,18 +64,53 @@ class _HomePageState extends State<HomePage> {
       body: ListView.builder(
         itemBuilder: (context, index) {
           Stock stock = _stockList[index];
-          return ListTile(
-            title: Text("${stock.symbol}"),
-            subtitle: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Status: ${stock.tradeStatus}"),
-                Text("CMP: ${stock.cmp} INR"),
-              ],
+          return Container(
+            margin: EdgeInsets.only(bottom: 4),
+            child: ListTile(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "${stock.symbol}",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline4!
+                        .copyWith(color: Colors.black),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "CMP: ${stock.cmp}",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1!
+                            .copyWith(color: Colors.black),
+                      ),
+                      Text(
+                        "Stop Loss: ${stock.sl}",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1!
+                            .copyWith(color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              subtitle: Text(
+                "Status: ${stock.tradeStatus}",
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1!
+                    .copyWith(color: Colors.black),
+              ),
+              onTap: () {
+                pushScreen(StockPage(stock: stock), context);
+              },
+              tileColor: _tileColor(stock.tradeStatus!),
             ),
-            onTap: () {
-              pushScreen(StockPage(stock: stock), context);
-            },
           );
         },
         itemCount: _stockList.length,
